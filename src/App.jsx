5a295a2c645f8adcd99a1d0b9592106e2bea8bdd
Sandip123samanta <111598408+Sandip123samanta@users.noreply.github.com>
2024-12-02 16:44:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from './components/Hero';
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+      <section className="min-h-screen"></section>
+    </main>
+  );
+};
+
+export default App;
